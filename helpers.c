1c724384ddc966deb24e50028c8c8fa4208b6843
arjungoyal99 <arjungoyal99@users.noreply.github.com>
2017-06-21 20:46:08 +0000

    20170621T204524Z

---
+++ helpers.c
@@ -0,0 +1,61 @@
+/**
+ * helpers.c
+ *
+ * Helper functions for Problem Set 3.
+ */
+ 
+#include <cs50.h>
+
+#include "helpers.h"
+
+/**
+ * Returns true if value is in array of n values, else false.
+ */
+bool search(int value, int values[], int n)
+{
+    // TODO: implement a searching algorithm
+    if (n<0)
+    {
+        return false;
+    }
+    int beg=0;
+    int last = n;
+    while (beg<=last)
+    {
+        int mid = (beg+last)/2;
+        if (values[mid]==value)
+        {
+            return true;
+        }
+        else if (values[mid]>value)
+        {
+            last = mid-1;
+        }
+        else if (values[mid]<value)
+        {
+            beg= mid+1;
+        }
+    }
+    return false;
+}
+
+/**
+ * Sorts array of n values.
+ */
+void sort(int values[], int n)
+{
+    // TODO: implement a sorting algorithm
+    for (int i=0; i<n-1; i++)
+    {
+        for (int j=0; j<n-1-i; j++)
+        {
+            if (values[j]>values[j+1])
+            {
+                int temp=values[j];
+                values[j]=values[j+1];
+                values[j+1]=temp;
+            }
+            
+        }
+    }
+}
